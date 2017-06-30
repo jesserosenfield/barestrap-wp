6968884268344a6b6d004520795119eac142a9a2
Jesse <jsros86@gmail.com>
2017-06-29 23:48:08 -0400

    unsplash

--- wp-content/themes/handistrap-wp/header.php
+++ wp-content/themes/handistrap-wp/header.php
@@ -13,59 +13,8 @@ $GLOBALS[ 'themename' ] = 'handistrap-wp';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-$header_files = array( $ssd  . "/assets/css/header.less" => $ssd );
-$framework_files = array( $ssd  . "/assets/css/framework.less" => $ssd );
-$below_files = array( $ssd  . "/assets/css/below-fold.less" => $ssd );
-
-$options = array(
-	'cache_dir' =>  $ssd . "/assets/css/cache/",
-	'compress' => true,
-    'sourceMap'         => true,
-    'sourceMapWriteTo'  => $ssd  . '/assets/css/map.map',
-    'sourceMapURL'      => $ssd . '/assets/css/map.map',
-);
-
-$framework_file_name = Less_Cache::Get( $framework_files, $options );
-$header_file_name = Less_Cache::Get( $header_files, $options );
-$below_file_name = Less_Cache::Get( $below_files, $options );
-
-/*
-try {
-	lessc::ccompile("library/themes/{$themename}/assets/css/style-me.less", "library/themes/{$themename}/{$themename}.css");
-	lessc::ccompile("library/themes/{$themename}/assets/css/style-me-all.less", "library/themes/{$themename}/{$themename}-all.css");
-} catch (exception $ex) {
-	exit('lessc fatal error:<br />'.$ex->getMessage());
-}
-*/
-
-//$GLOBALS[ 'critical_css' ] = get_field('critical_css', 'option');
-
-$GLOBALS[ 'testing' ] = true; 
-
-
-
-$fav = get_field('favicon', 'option');
-$fav = $fav['id'];
-
-$fav16 = wp_get_attachment_image_src($fav, 'fav16');
-$fav32 = wp_get_attachment_image_src($fav, 'fav32');
-$fav96 = wp_get_attachment_image_src($fav, 'fav96');
-$fav196 = wp_get_attachment_image_src($fav, 'fav196');
-
-$fav57 = wp_get_attachment_image_src($fav, 'fav57');
-$fav60 = wp_get_attachment_image_src($fav, 'fav60');
-$fav72 = wp_get_attachment_image_src($fav, 'fav72');
-$fav76 = wp_get_attachment_image_src($fav, 'fav76');
-$fav114 = wp_get_attachment_image_src($fav, 'fav114');
-$fav120 = wp_get_attachment_image_src($fav, 'fav120');
-$fav144 = wp_get_attachment_image_src($fav, 'fav144');
-$fav152 = wp_get_attachment_image_src($fav, 'fav152');
-$fav180 = wp_get_attachment_image_src($fav, 'fav180');
-$fav192 = wp_get_attachment_image_src($fav, 'fav192');
-=======
 include('templates/header/less.php');
 include('templates/header/favicon.php');
->>>>>>> 5ec886a4030c0e55cd610834e2d6ce8b1229300f
 ?>
 
 <!DOCTYPE HTML>
@@ -196,4 +145,3 @@ include('templates/header/favicon.php');
 			</header> <!-- header -->
 			
 			<div id="switcheroo" class="ajax-fade">
-			
